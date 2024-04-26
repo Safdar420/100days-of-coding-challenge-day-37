0ff41c70b1939cedd5950a94f231bd053bc1cb93
Safdar420 <160203243+Safdar420@users.noreply.github.com>
2024-04-26 20:36:26 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+// Q no 109
+var currentTime = new Date();
+if (currentTime.getHours() < 12) {
+    console.log("Good Morning");
+}
+//  Q    no 110
+function assignGrade(score) {
+    if (score >= 90) {
+        return "A";
+    }
+    else if (score >= 80) {
+        return "B";
+    }
+    else if (score >= 70) {
+        return "C";
+    }
+    else if (score >= 60) {
+        return "D";
+    }
+    else {
+        return "F";
+    }
+}
+console.log(assignGrade(85));
+console.log(assignGrade(55));
+//  Q no 111
+function catagorizeAge(age) {
+    if (age < 13) {
+        return "Child";
+    }
+    else if (age <= 19) {
+        return "Teenager";
+    }
+    else {
+        return "Adult";
+    }
+}
+console.log(catagorizeAge(12));
+console.log(catagorizeAge(18));
+console.log(catagorizeAge(25));
